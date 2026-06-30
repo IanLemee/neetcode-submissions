604c0393901ffbc67be0d48baa38d2a6bc70b90d
Ian Leme <130516228+IanLemee@users.noreply.github.com>
2026-06-30 16:34:32 -0300

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int row = 0; row < board.length; row++) {
+            for(int col = 0; col < board[0].length; col++) {
+                if(!check(board, row, col) && board[row][col] != '.') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean check(char[][] board, int row, int col) {
+        char ch = board[row][col];
+        for(int r = 0; r < board.length; r++) {
+            if(r == row) {
+                continue;
+            }
+            if(board[r][col] == ch) return false;
+        }
+
+        for(int c = 0; c < board.length; c++) {
+            if(c == col) {
+                continue;
+            }
+            if(board[row][c] == ch) return false;
+        }
+
+        int k = ((row / 3 ) * 3) + (col /3);
+        int startRow = (k / 3) * 3;
+        int startCol = (k % 3) * 3;
+        
+        for(int r = startRow; r < startRow + 2; r++) {
+            for(int c = startCol; c < startCol + 2; c++) {
+                if(r == row && c == col) {
+                    continue;
+                }
+                
+            if(board[r][c] == ch) return false;
+            }
+        }
+        return true;
+    }
+}
